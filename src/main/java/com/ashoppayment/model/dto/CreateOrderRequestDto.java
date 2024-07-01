007224068bdd5f384fdbb277b485a24db95d8bcc
Evgeny Shurupov <schuevgeny@yandex.ru>
2024-07-01 10:39:16 +0300

    Config Consumer. Notwork

--- src/main/java/com/ashoppayment/model/dto/CreateOrderRequestDto.java
+++ src/main/java/com/ashoppayment/model/dto/CreateOrderRequestDto.java
@@ -2,9 +2,11 @@ package com.ashoppayment.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateOrderRequestDto {
     private long orderId;
     private long userId;
