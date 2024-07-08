46a3f76c0f46e0b7b433ea4c9498b1c814f7b6bf
Evgeny Shurupov <schuevgeny@yandex.ru>
2024-07-08 15:08:30 +0300

    Fix listener

--- src/main/java/com/ashoppayment/messaging/listener/impl/MessageListenerImpl.java
+++ src/main/java/com/ashoppayment/messaging/listener/impl/MessageListenerImpl.java
@@ -1,11 +1,12 @@
 package com.ashoppayment.messaging.listener.impl;
 
-import com.ashoppayment.messaging.listener.MessageListener;
 import com.ashoppayment.model.dto.CreateOrderRequestDto;
 import com.ashoppayment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 
@@ -22,7 +23,7 @@ public class MessageListenerImpl /*implements MessageListener*/ {
             groupId = "payment-group",
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void listenMessage(String key, CreateOrderRequestDto createOrderRequestDto) {
+    public void listenMessage(@Header(KafkaHeaders.RECEIVED_KEY) String key, CreateOrderRequestDto createOrderRequestDto) {
             log.info("Received order message from orders service with key: {}", key);
             paymentService.payment(createOrderRequestDto);
     }
